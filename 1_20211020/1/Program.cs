0b0291b4ee92c7f1bd18198f07d69072b4e88a67
S240428037 <43860827+240428037@users.noreply.github.com>
2021-10-20 21:41:18 +0800

    push

---
+++ 1_20211020/1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//namespace _1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+//}
+
+//namespace _1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Calculator c = new Calculator();
+//            Console.WriteLine(c.GetConeVolume(3, 4));
+//        }
+//    }
+//    class Calculator
+//    {
+//        public double GetCircleArea(double r)
+//        {
+//            return Math.PI * r * r;
+//        }
+//        public double GetCylinderVolume(double r, double h)
+//        {
+//            double a=GetCircleArea(r)
+//            return a * h;
+//        }
+//        public double GetConeVolume(double r, double h)
+//        {
+//            double cv = GetCylinderVolume(r, h);
+//            return  cv/ 3;
+//        }
+//    }
+//}
+
+
+//namespace _1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student stu1 = new Student();
+//            Console.WriteLine(stu1.ID);//默认-0;无参-1
+//            Console.WriteLine(stu1.Name);//默认-null;无参-No name
+//            //-
+//            //带参数的构造器
+//            Student stu2 = new Student(2, "Tim");
+//            Console.WriteLine(stu2.ID);//1
+//            Console.WriteLine(stu2.Name);//Tim
+//        }
+//    }
+//    class Student
+//    {
+//        //无参的构造器
+//        public Student()
+//        {
+//            this.ID = 1;
+//            this.Name = "No name";
+//        }
+//        //-
+//        //带参的构造器
+//        public Student(int initID,string initName)
+//        {
+//            this.ID = initID;
+//            this.Name = initName;
+//        }
+//        public int ID;
+//        public string Name;
+//    }
+//}
+
+
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
